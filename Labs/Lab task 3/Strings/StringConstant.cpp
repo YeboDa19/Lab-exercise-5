084d7776e68ddd0f742224e0525fbf2e6a531d10
YeboDa19 <bsc-com-18-22@unima.ac.mw>
2024-04-30 10:17:25 +0200

    LAB TASK 3 : STRINGS

---
+++ Labs/Lab task 3/Strings/StringConstant.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    char greeting[6] = {'H', 'E', 'L', 'L', 'O', '\0'};
+
+    cout << "gretting message : ";
+    cout << greeting << endl;
+    return 0;
+}
